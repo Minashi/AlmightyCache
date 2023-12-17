6be476893a51d64d2d476e75828357f2d9cd4577
Brandon Gonzalez <nimdaminashi@gmail.com>
2023-12-17 15:35:21 -0500

    Update firewall.sh

--- Scripts/ccdc/firewall.sh
+++ Scripts/ccdc/firewall.sh
@@ -1,1 +1,34 @@
+# Firewall setup
 
+# Verify UFW is installed
+sudo apt install ufw
+sleep 2
+sudo ufw disable
+
+# Setting up default UFW Policies
+sudo ufw default deny incoming
+sudo ufw default allow outgoing
+
+# Setting up specific service policies
+sudo ufw allow ssh
+# sudo ufw allow http
+# sudo ufw allow https
+
+# Allowing/denying from specific IP addresses
+# sudo ufw allow from IP
+# sudo ufw deny from IP
+# sudo ufw allow from IP to any port PORT
+
+# Allowing a range of ports
+# sudo ufw allow 2000:2004/tcp
+# sudo ufw allow 2000:2004/udp
+
+# Deleting UFW rules
+# sudo ufw status numbered
+# sudo ufw delete NUM
+
+# Enable the UFW service
+sudo ufw enable
+
+# Display established policies
+sudo ufw status verbose
